e20cca9670c80e0f9eb4dddd95fe718a14454316
Casey Milne <casey.milne@eatbuildplay.com>
2021-08-29 11:41:40 -0500

    Update version to 1.0.1.

--- barcode-man-for-woocommerce.php
+++ barcode-man-for-woocommerce.php
@@ -4,7 +4,7 @@
 Plugin Name: Barcode Man for WooCommerce
 Plugin URI: https://saberwp.com/plugins/barcode-man-wc/
 Description: Connect to the Bar Code Man service for WooCommerce.
-Version: 1.0.0
+Version: 1.0.1
 Author: Barcode Man
 Author URI: https://saberwp.com/
 License: GPL2 http://www.gnu.org/licenses/gpl-2.0.html
@@ -14,15 +14,13 @@ WC tested up to: 5.5
 */
 
 define( 'BARCODEMAN_PLUGIN_NAME', 'Barcode Man' );
-define( 'BARCODEMAN_VERSION', '1.4.1' );
+define( 'BARCODEMAN_VERSION', '1.0.1' );
 define( 'BARCODEMAN_PATH', plugin_dir_path(__FILE__) );
 define( 'BARCODEMAN_URL', plugin_dir_url(__FILE__) );
 define( 'BARCODEMAN_DEV_MODE', 0 );
 define( 'BARCODEMAN_TEXT_DOMAIN', 'barcodeman' );
 
 class BarcodeMan_Base {
-
-	const VERSION = '1.0.0';
 
 	// Construct the plugin
 	public function __construct() {
