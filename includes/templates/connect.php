369533de96b103a0dc11705f957e6284f9410f64
willyou <mrjump0204@gmail.com>
2021-09-24 10:48:58 +0800

    update image

--- includes/templates/connect.php
+++ includes/templates/connect.php
@@ -4,7 +4,7 @@
 
 		<h1><?php esc_html_e('Connect to Barcodeman', 'barcodeman'); ?></h1>
 
-		<img src=" <?php echo BARCODEMAN_URL; ?>/assets/images/connect.svg" class="connect-image" alt="connect to barcodeman">
+		<img src=" <?php echo BARCODEMAN_URL; ?>/assets/images/connect.png" class="connect-image" alt="connect to barcodeman">
 		<p class="connect-description"><?php esc_html_e('You\'re almost done! Just 2 more steps to have your WooCommerce store connected to BarcodeMan.', 'barcodeman'); ?></p>
 		<?php echo '<a href="https://app.barcodeman.app/" class="button button-primary barcodeman-connect-button" target="_blank">' . esc_html__('Connect', 'barcodeman') . '</a>'; ?>
 
