177a8ba6eac13c3c083ae80201f18d3f508dc942
Casey Milne <casey.milne@eatbuildplay.com>
2021-11-10 20:45:39 -0600

    Fixing PHPCS errors in header template.

--- includes/templates/header.php
+++ includes/templates/header.php
@@ -1,20 +1,31 @@
+<?php
+/**
+ * Support template.
+ *
+ * @package Barcodeman
+ */
+
+?>
+
 <div class="wrap">
 
-    <?php
-    $base_url = '?page=barcodeman-dashboard';
-    ?>
+<?php
+	$base_url = '?page=barcodeman-dashboard';
+?>
 
-    <h2 class="nav-tab-wrapper barcodeman-tabs">
-        <?php foreach ($tabs as $tab) : ?>
-            <?php
-	        $active = '';
-	        if ( ! empty( $_GET['tab'] ) && $_GET['tab'] == $tab['tab_url'] ) {
-		        $active = 'nav-tab-active';
-	        }
-	        if ( empty( $_GET['tab'] ) && $tab['tab_url'] == '' ) {
-		        $active = 'nav-tab-active';
-	        }
-            ?>
-            <a href="<?php echo esc_url($base_url . ($tab['tab_url'] ? '&tab=' . $tab['tab_url'] : '') ); ?>" class="nav-tab <?php echo esc_attr($active);?>"><?php echo esc_html($tab['name']); ?></a>
-        <?php endforeach; ?>
-    </h2>
+<h2 class="nav-tab-wrapper barcodeman-tabs">
+	<?php foreach ( $tabs as $tab_item_item ) : ?>
+		<?php
+
+		$active = '';
+		if ( ! empty( $_GET['tab'] ) && $_GET['tab'] === $tab_item['tab_url'] ) {
+			$active = 'nav-tab-active';
+		}
+		if ( empty( $_GET['tab'] ) && '' === $tab_item['tab_url'] ) {
+			$active = 'nav-tab-active';
+		}
+		?>
+
+	<a href="<?php echo esc_url( $base_url . ( $tab_item['tab_url'] ? '&tab=' . $tab_item['tab_url'] : '' ) ); ?>" class="nav-tab <?php echo esc_attr( $active ); ?>"><?php echo esc_html( $tab_item['name'] ); ?></a>
+	<?php endforeach; ?>
+</h2>
